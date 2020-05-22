d3065f4525a938f7ba6750e1cefc30a363290334
Johnatas V. Marques <johumar@users.noreply.github.com>
2020-05-21 22:16:50 -0300

    inicial

---
+++ Controllers/TabuadaController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcGettingStarte.Controllers
+{
+
+    public class TabuadaController : Controller
+    {
+        public IActionResult Calcular(){
+            int[] resultado = new int [10];
+            for(int i = 1; i <= 10; i ++)
+             resultado[i-1] = 5 * i;
+             return View (resultado);
+        }
+
+    }
+
+
+}
